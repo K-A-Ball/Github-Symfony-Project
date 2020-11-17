321b32b4f5108616723106d4b0e0efdaf7a12f3f
K-A-Ball <58625947+K-A-Ball@users.noreply.github.com>
2020-11-17 23:33:32 +0000

    Handling invalid searches, splitting up methods for readability, and calculating time since profile was last updated
    Methods split into more readable chunks, invalid searches are displayed to the user via the textarea and then validated again when the input value has changed, and calculating time since profile was last updated

--- assets/components/userInformationTable.js
+++ assets/components/userInformationTable.js
@@ -7,6 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
 import axios from 'axios'
 
 
@@ -18,41 +19,95 @@ const useStyles = makeStyles({
 
 export default function UserInformationTable() {
   const classes = useStyles();
-  const [user_to_search, setUserToSearch] = useState("")
-  const [user_data, setUserData] = useState([])
-  const [cached_searches, setCachedSearches] = useState([])
-  const keys_to_display = ["login", "name", "public_repos", "followers", "following", "updated_at"]
+  const [user_to_search, setUserToSearch] = useState("");
+  const [user_data, setUserData] = useState([]);
+  const [cached_searches, setCachedSearches] = useState([]);
+  const [cached_updated_profile_times, SetUpdatedProfileTimes] = useState([])
+  const [updated_profile_time, setUpdatedProfileTime] = useState("")
+  const [invalid_searches, setInvalidSearches] = useState([]);
+  const [invalid_search, setInvalidSearch] = useState(false);
+  const keys_to_display = ["login", "name", "public_repos", "followers", "following"]
 
   const handleSearch = (user_string) => {
     const previous_search_data = cached_searches.filter(search_dict => Object.keys(search_dict).includes(user_string))
-    previous_search_data.length > 0 ? setUserData(previous_search_data[0][user_string]) : getUserDataFromApi(user_string)
+    if (previous_search_data.length > 0) {
+      setUserData(Object.entries(previous_search_data[0][user_string]).filter(([key, value]) => keys_to_display.includes(key)))
+      findAndSetUpdatedProfileTime(previous_search_data[0][user_string])
+    } else {
+      getUserDataFromApi(user_string)
+    }
+  }
+
+  const findAndSetUpdatedProfileTime = (user_data) => {
+    const [_, updated_profile_time] = Object.entries(user_data).filter(([key, value]) => key === "updated_at").flat()
+    setUpdatedProfileTime(updated_profile_time)
+  }
+
+
+  const cacheUserData = (user_data, user_string, cache_type) => {
+    const user_dict = {}
+    user_dict[user_string] = user_data;
+    setCachedSearches([...cached_searches, user_dict])
+  }
+
+  const setDataToDisplayAndCache = (user_data, user_string) => {
+    const filtered_user_data = Object.entries(user_data).filter(([key, value]) => keys_to_display.includes(key))
+    const [_, updated_profile_time] = Object.entries(user_data).filter(([key, value]) => key === "updated_at").flat()
+    setUpdatedProfileTime(updated_profile_time)
+    setUserData(filtered_user_data)
+    cacheUserData(user_data, user_string)
+  }
+
+  const setSearchAsInvalidAndCache = (username) => {
+    setInvalidSearch(true);
+    setInvalidSearches([...invalid_searches, username])
   }
 
   const getUserDataFromApi = (user_string) => {
-    axios.get(`/profiles/${user_string}`).then(response => {
-      const filtered_user_data = Object.entries(response.data).filter(([key, value]) => keys_to_display.includes(key))
-      setUserData(filtered_user_data)
-      const user_dict = {}
-      user_dict[user_string] = filtered_user_data;
-      setCachedSearches([...cached_searches, user_dict])
-    }).catch(error => {
-      console.log(error)
-    })
+    if (invalid_searches.includes(user_string)) {
+      setInvalidSearch(true)
+    } else {
+      axios.get(`/profiles/${user_string}`).then(response => {
+        response.data.message === "Not Found" ? setSearchAsInvalidAndCache(user_string) : setDataToDisplayAndCache(response.data, user_string)
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  }
+
+  const handleSearchInput = (e) => {
+    !invalid_searches.includes(e.target.value) ? setInvalidSearch(false) : null;
+    setUserToSearch(e.target.value)
+  }
+
+  const calculateWhenProfileLastUpdated = (last_updated_time) => {
+    if (last_updated_time !== "") {
+      const current_time = new Date()
+      const difference = current_time - new Date(last_updated_time)
+      return Math.floor(difference / 86400000);
+    }
   }
 
   return (
     <div>
-      <input onChange={(e) => setUserToSearch(e.target.value)} />
-      <button onClick={(e) => handleSearch(user_to_search)}> Search </button>
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="simple table">
           <TableHead>
+            <TableRow className="user_search_row">
+              <TableCell>
+                <TextField onChange={(e) => handleSearchInput(e)} error={invalid_search} label={invalid_search ? "User not found" : "Search for a user"} />
+                <button onClick={(e) => handleSearch(user_to_search)}> Search </button>
+              </TableCell>
+            </TableRow>
             <TableRow>
               {keys_to_display.map(key =>
                 <TableCell key={key}>
                   {key}
                 </TableCell>
               )}
+              <TableCell>
+                Days since last profile update
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -62,6 +117,9 @@ export default function UserInformationTable() {
                   {value === null ? "Not supplied by user" : value}
                 </TableCell>
               )}
+              <TableCell>
+                {calculateWhenProfileLastUpdated(updated_profile_time)}
+              </TableCell>
             </TableRow>
           </TableBody>
         </Table>
