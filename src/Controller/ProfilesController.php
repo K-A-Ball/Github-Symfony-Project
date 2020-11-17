324933c06b320c1b1187c6390297149d36194f04
K-A-Ball <58625947+K-A-Ball@users.noreply.github.com>
2020-11-17 19:54:17 +0000

    Initial Commit
    Basic setup of the controllers, a generic placeholder rendered for the frontend, and basic curl set up

---
+++ src/Controller/ProfilesController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Config\Definition\Exception\Exception;
+class ProfilesController extends AbstractController {
+    
+    /**
+     * @Route("/profiles", name="profiles")
+     */
+        public function index(): Response
+    {
+        return $this->render('profiles/index.html.twig', [
+            'controller_name' => 'ProfilesController',
+        ]);
+    }
+
+    /**
+     * @Route("/profiles/{username}", name="user_profile_search")
+     */
+
+    public function update(): Response
+    {
+        // get base from url and extract it as username var
+        $uri = $_SERVER['REQUEST_URI'];
+        $username = basename($uri);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, "https://api.github.com/users/$username");
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_USERAGENT, "Accessing Github user.");
+        $result = curl_exec($curl);
+        $json = json_decode($result, 1);
+        $resources = $json["updated_at"];
+
+        return $this->JSON($resources);
+    }
+
+}
